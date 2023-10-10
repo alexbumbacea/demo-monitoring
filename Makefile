30755992f894ce49a7c27b67c3ddc7aaa19b7f2c
Alexandru Bumbacea <alex.bumbacea@gmail.com>
2023-10-10 23:13:14 +0300

    Running multiple instances and version of python

--- Makefile
+++ Makefile
@@ -15,3 +15,11 @@ build.%:
 logs.%: SERVICE=$*
 logs.%:
 	docker-compose logs -f --tail 100 $(SERVICE)
+stop:
+	docker-compose stop
+start-testing:
+	ab -n 10000 -c 20 -t 600 http://localhost/python36/ &
+	ab -n 10000 -c 20 -t 600 http://localhost/python35/ &
+	ab -n 10000 -m POST -c 20 -t 600 http://localhost/python35/ &
+	ab -n 10000 -c 20 -t 600 http://localhost/php/ &
+	ab -n 10000 -m PATCH -c 20 -t 600 http://localhost/php/ &
--- python/main.py
+++ python/main.py
@@ -24,16 +24,10 @@ class S(BaseHTTPRequestHandler):
         )
 
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response("GET")
         self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
     def do_POST(self):
-        # content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        # post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        # logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-        #              str(self.path), str(self.headers), post_data.decode('utf-8'))
-
         self._set_response("POST")
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
